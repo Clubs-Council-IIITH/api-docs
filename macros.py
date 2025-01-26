b83762bdeb2c84648003d97931f6fa14293811d9
Bhav Beri <bhavberi@gmail.com>
2025-01-26 18:05:27 +0530

    Fix mkdocstring working with mkmacros (Based on https://github.com/mkdocstrings/mkdocstrings/issues/615)

---
+++ macros.py
@@ -0,0 +1,23 @@
+def define_env(env):
+    # get mkdocstrings' Python handler
+    python_handler = env.conf["plugins"]["mkdocstrings"].get_handler("python")
+ 
+    # get the `update_env` method of the Python handler
+    update_env = python_handler.update_env
+
+    # override the `update_env` method of the Python handler
+    def patched_update_env(md, config):
+        update_env(md, config)
+
+        # get the `convert_markdown` filter of the env
+        convert_markdown = python_handler.env.filters["convert_markdown"]
+
+        # build a chimera made of macros+mkdocstrings
+        def render_convert(markdown: str, *args, **kwargs):
+            return convert_markdown(env.render(markdown), *args, **kwargs)
+
+        # patch the filter
+        python_handler.env.filters["convert_markdown"] = render_convert
+    
+    # patch the method
+    python_handler.update_env = patched_update_env
